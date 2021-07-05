d3284b9a50bd31ffa170be20f8e6080ba31f51c0
UncleKhab <cobrosoffice@gmail.com>
2021-07-05 13:06:26 +0300

    useState

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "random message",
+  });
+  const changeMessage = () => {
+    // setPerson({ ...person, message: "Hello World" });
+    setMessage("hello world");
+  };
+
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const handleDecrease = () => {
+    setValue(value - 1);
+  };
+  const handleReset = () => {
+    setValue(0);
+  };
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={handleDecrease}>
+          Decrease
+        </button>
+        <button className="btn" onClick={handleReset}>
+          reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          incease later
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
